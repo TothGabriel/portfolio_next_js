b03071bb51cf91d4cce5eb84cfb3cb3065425af4
GabrielToth <gabt71@gmail.com>
2023-11-08 15:30:26 +0100

    Début de NavBar

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { NAV_LINKS } from "@constants"
+import Image from "next/image"
+import Link from "next/link"
+
+export const Navbar = () => {
+  return (
+    <nav className="flex flexBetween w-full  relative z-30 py-5 px-5">
+      <Link href="/">
+        <Image src="/" alt="logo" width={74} height={29} />
+      </Link>
+
+      
+      <ul className="hidden h-full gap-12 sm:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="dark:text-white light:text-black"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+    </nav>
+  );
+};
+
+export default Navbar;
